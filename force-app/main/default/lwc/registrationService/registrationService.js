be05f63f8421b4c20d9f92f179e166d4f2110395
Mikołaj Elgert <mikolajelgert@gmail.com>
2024-11-23 21:10:00 +0100

    rest class changes, lwc correction

--- force-app/main/default/lwc/registrationService/registrationService.js
+++ force-app/main/default/lwc/registrationService/registrationService.js
@@ -15,4 +15,16 @@ export default class RegistrationService extends NavigationMixin(LightningElemen
             }
         }, true)
     }
+
+    //createApex user, send template
+    handleSubmit(event) {
+        const inputs = this.template.querySelectorAll('lightning-input');
+        const formatData = {};
+
+        inputs.forEach(input => {
+            formatData[input.name] = input.value;
+        });
+        
+        console.log(JSON.stringify(formatData));
+    }
 }
